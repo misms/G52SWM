998278f028ff1c13f81e03556fda5f39bbdea8a2
Aiman796 <aimansopian96@gmail.com>
2016-12-19 15:00:41 +0800

    Added SceneBuilder Integration
    Added Controller Class: "TMEController.java"
    Added fxml file: "TME.fxml"

---
+++ Diamond Hunter/src/com/neet/DiamondHunter/TileMapEditor/TMEController.java
@@ -0,0 +1,14 @@
+package com.neet.DiamondHunter.TileMapEditor;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TMEController implements Initializable{
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
